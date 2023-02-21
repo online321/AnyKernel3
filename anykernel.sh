9dd8812b82e23509aee083521e165b45dec7e67a
online321 <grf5233@gmail.com>
2023-02-21 19:20:57 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,64 +1,46 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
+### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=aRiseKernel By Prashant
+kernel.string=N0Kernel-KSU
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
+device.name1=alioth
+device.name2=aliothin
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
-# shell variables
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+} # end attributes
+
+
+## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-## AnyKernel boot install
-dump_boot;
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
-## begin ramdisk changes
-# Set Android version for kernel
-ver="$(file_getprop /system/build.prop ro.build.version.release)"
-if [ ! -z "$ver" ]; then
-    patch_cmdline "androidboot.version" "androidboot.version=$ver"
-else
-    patch_cmdline "androidboot.version" ""
-fi
-
-write_boot;
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
 
